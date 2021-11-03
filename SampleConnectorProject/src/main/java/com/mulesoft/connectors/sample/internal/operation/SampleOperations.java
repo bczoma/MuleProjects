7d17823fea540d247b1c1547f05221796b627c0a
bczoma <balazs.czoma@solace.com>
2021-11-03 11:24:13 -0400

    Cleanup

--- SampleConnectorProject/src/main/java/com/mulesoft/connectors/sample/internal/operation/SampleOperations.java
+++ SampleConnectorProject/src/main/java/com/mulesoft/connectors/sample/internal/operation/SampleOperations.java
@@ -14,16 +14,22 @@ import org.mule.runtime.extension.api.annotation.param.ParameterGroup;
 import com.mulesoft.connectors.sample.api.ActionIdentifier;
 import com.mulesoft.connectors.sample.internal.config.SampleConfiguration;
 import com.mulesoft.connectors.sample.internal.metadata.InputTypeResolverOperationWithPartialFetch;
-import com.mulesoft.connectors.sample.internal.metadata.OutputTypeResolverOperation;
 import com.mulesoft.connectors.sample.internal.metadata.OutputTypeResolverOperationWithPartialFetch;
-import com.mulesoft.connectors.sample.internal.metadata.TypeKeysResolverOperation;
 import com.mulesoft.connectors.sample.internal.metadata.TypeKeysResolverOperationWithPartialFetch;
 
 public class SampleOperations {
 
+  
+  /*
+   * Partial Fetching Demo
+   * 
+   * Refer to https://docs.mulesoft.com/mule-sdk/1.1/multi-level-metadata#partial-fetching
+   */
+  
+  
   @OutputResolver(output = OutputTypeResolverOperationWithPartialFetch.class)
   @MediaType(value = ANY, strict = false)
-  public Object sampleOperation1( /* NO @Config SampleConfiguration configuration, */
+  public Object partialFetchingWorksOperation( /* NO @Config SampleConfiguration configuration, */
       @ParameterGroup(name = "MetadataKey") @MetadataKeyId(TypeKeysResolverOperationWithPartialFetch.class) ActionIdentifier identifier,
       @Content(primary = true) @TypeResolver(InputTypeResolverOperationWithPartialFetch.class) TypedValue<Object> otherArg){
     return null;
@@ -31,7 +37,7 @@ public class SampleOperations {
   
   @OutputResolver(output = OutputTypeResolverOperationWithPartialFetch.class)
   @MediaType(value = ANY, strict = false)
-  public Object sampleOperation2( @Config SampleConfiguration configuration,
+  public Object partialFetchingDoesNotWorkOperation( @Config SampleConfiguration configuration,
       @ParameterGroup(name = "MetadataKey") @MetadataKeyId(TypeKeysResolverOperationWithPartialFetch.class) ActionIdentifier identifier,
       @Content(primary = true) @TypeResolver(InputTypeResolverOperationWithPartialFetch.class) TypedValue<Object> otherArg){
     return null;

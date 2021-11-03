9f049847b3679125844fc1ae1a2073417a12fdf5
bczoma <balazs.czoma@solace.com>
2021-11-03 08:55:55 -0400

    Updated sample connector project POM to match latest versions

--- SampleConnectorProject/src/main/java/com/mulesoft/connectors/sample/internal/config/SampleConfiguration.java
+++ SampleConnectorProject/src/main/java/com/mulesoft/connectors/sample/internal/config/SampleConfiguration.java
@@ -1,11 +1,12 @@
 package com.mulesoft.connectors.sample.internal.config;
 
+import org.mule.runtime.api.lifecycle.Initialisable;
+import org.mule.runtime.api.lifecycle.InitialisationException;
 import org.mule.runtime.extension.api.annotation.Configuration;
 import org.mule.runtime.extension.api.annotation.Operations;
 import org.mule.runtime.extension.api.annotation.Sources;
 import org.mule.runtime.extension.api.annotation.connectivity.ConnectionProviders;
 
-import com.mulesoft.connectivity.rest.commons.api.configuration.RestConfiguration;
 import com.mulesoft.connectors.sample.internal.connection.provider.UnsecuredConnectionProvider;
 import com.mulesoft.connectors.sample.internal.operation.SampleOperations;
 import com.mulesoft.connectors.sample.internal.source.SampleSource;
@@ -14,4 +15,9 @@ import com.mulesoft.connectors.sample.internal.source.SampleSource;
 @Operations({SampleOperations.class})
 @Sources({ SampleSource.class })
 @ConnectionProviders({UnsecuredConnectionProvider.class})
-public class SampleConfiguration extends RestConfiguration {}
+public class SampleConfiguration implements Initialisable {
+
+  @Override
+  public void initialise() throws InitialisationException {
+  }
+}
